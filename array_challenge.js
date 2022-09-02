da39e4419e99035e65b7755f767144ba01b6e6c8
belovetech <belovetech@gmail.com>
2022-09-02 21:18:13 +0100

    Use chaining methods to solve previous challenge

--- array_challenge.js
+++ array_challenge.js
@@ -60,3 +60,18 @@ const calcAverageHumanAge = function (ages) {
 const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
 const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
 console.log(avg1, avg2);
+
+// Chaining methods
+console.log(`\n_____Challenge 3______`);
+const calcAverageHumanAge2 = function (ages) {
+  const average = ages
+    .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
+    .filter(age => age >= 18)
+    .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
+
+  return average;
+};
+
+const avg3 = calcAverageHumanAge2([5, 2, 4, 1, 15, 8, 3]);
+const avg4 = calcAverageHumanAge2([16, 6, 10, 5, 6, 1, 4]);
+console.log(avg3, avg4);
